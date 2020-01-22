3e3046ddd43ed1f8ab89955f7f801e0194449694
bluesky0125 <time2go@yeah.net>
2020-01-22 19:28:12 +0800

    add hello project

---
+++ hello/multiple/libHelloSLAM.h
@@ -0,0 +1,6 @@
+
+#ifndef LIBHELLOSLAM_H
+#define LIBhelloSLAM_H
+// 上面的宏定义是为了防止重复引用这个头文件而引起的重定义错误
+void printHello();
+#endif
---
+++ hello/multiple/useHello.cpp
@@ -0,0 +1,6 @@
+#include "libHelloSLAM.h"    //调用库文件
+int main(int atgc,char** argv)
+{
+    printHello();
+    return 0;
+}
