78016c36eb8dfbfdd588f7d466b5c69a6bc1a8d2
bluesky0125 <time2go@yeah.net>
2020-01-28 16:46:16 +0800

    update

---
+++ hello/complex/hello/hello.h
@@ -0,0 +1,5 @@
+#ifndef LIBHELLO_H
+#define LIBHELLO_H
+// 上面的宏定义是为了防止重复引用这个头文件而引起的重定义错误
+void printHello();
+#endif
---
+++ hello/complex/main.cpp
@@ -0,0 +1,7 @@
+
+#include "hello.h"    //调用库文件
+int main(int atgc,char** argv)
+{
+    printHello();
+    return 0;
+}
